0bdefc200eb354605b2d837d99446aa540b69248
abr-ya <gamer05@list.ru>
2020-07-22 08:54:36 +0300

    Add Movie component

--- src/api.js
+++ src/api.js
@@ -1,27 +1,45 @@
 import { 
-  POPULAR_BASE_URL,
-  SEARCH_BASE_URL,
-  API_URL,
-  MOVIE_ENDPOINT,
-  CREDITS_ENDPOINT
+	POPULAR_BASE_URL,
+	SEARCH_BASE_URL,
+	API_URL,
+	MOVIE_ENDPOINT,
+	CREDITS_ENDPOINT
 } from './config';
 
 export const fetchMovies = async (movies, loadMore, searchTerm) => {
-  const endpoint = searchTerm 
-    ? `${SEARCH_BASE_URL}${searchTerm}&page=${
-      loadMore ? movies.currentPage + 1 : 1
-    }`
-    : `${POPULAR_BASE_URL}&page=${loadMore ? movies.currentPage + 1 : 1}`;
+	const endpoint = searchTerm 
+		? `${SEARCH_BASE_URL}${searchTerm}&page=${
+			loadMore ? movies.currentPage + 1 : 1
+		}`
+		: `${POPULAR_BASE_URL}&page=${loadMore ? movies.currentPage + 1 : 1}`;
 
-  const result = await (await fetch(endpoint)).json();
+	const result = await (await fetch(endpoint)).json();
 
-  return {
-    ...movies,
-    movies: loadMore 
-      ? [...movies.movies, ...result.results]
-      : [...result.results],
-    heroImage: result.results[0], // movies.heroImage || result.results[0],
-    currentPage: result.page,
-    totalPages: result.total_pages,
-  }
+	return {
+		...movies,
+		movies: loadMore 
+			? [...movies.movies, ...result.results]
+			: [...result.results],
+		heroImage: result.results[0], // movies.heroImage || result.results[0],
+		currentPage: result.page,
+		totalPages: result.total_pages,
+	}
 }
+
+export const fetchMovie = async (movieId) => {
+	const endpoint = MOVIE_ENDPOINT(movieId);
+	const creditsEndpoint = CREDITS_ENDPOINT(movieId);
+
+	const result = await (await fetch(endpoint)).json();
+	const credits = await (await fetch(creditsEndpoint)).json();
+
+	const directors = credits.crew.filter(
+		member => member.job === 'Director'
+	);
+
+	return {
+		...result,
+		actors: credits.cast,
+		directors,
+	}
+};
